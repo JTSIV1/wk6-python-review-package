589dbd0cd5d311e520c2304c88011672f400097f
jtsiv1 <johnspeaksiv@gmail.com>
2025-10-06 21:26:39 -0400

    add setup

---
+++ ai_ppp_python_review_solutions/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ai_ppp_python_review_solutions',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your package requires
+    ],
+)
